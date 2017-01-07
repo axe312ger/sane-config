5ea5ff5b8973e25c82af9b427a0a362b3b545bd5
Benedikt Rötsch <info@benedikt-roetsch.de>
2017-01-07 17:36:57 +0100

    fix(imports): fix typo in debug require

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const { resolve } = require('path')
 const { readdirSync } = require('fs')
 const { argv } = require('yargs')
 const findRoot = require('find-root')
-const Debug = require('Debug')
+const Debug = require('debug')
 const { validate } = require('jsonschema')
 
 const debug = Debug('config')
